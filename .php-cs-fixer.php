a22c0fcf385b9d619f2f5dedc8845ce03ec5b2ef
Viktoras Bezaras <viktoras.bezaras@gmail.com>
2022-04-27 21:52:59 +0200

    Updated dependencies

---
+++ .php-cs-fixer.php
@@ -0,0 +1,28 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__ . '/src');
+
+return (new PhpCsFixer\Config())
+    ->setRules(
+        [
+            '@PSR2'                                 => true,
+            '@Symfony'                              => true,
+
+            // Change Symfony parameters
+            'no_superfluous_phpdoc_tags'            => false,
+            'binary_operator_spaces'                => ['default' => 'align'],
+            'concat_space'                          => ['spacing' => 'one'],
+            'no_trailing_comma_in_singleline_array' => true,
+            'trailing_comma_in_multiline'           => false,
+            'yoda_style'                            => false,
+
+            // Additional rules
+            'align_multiline_comment'               => true,
+            'linebreak_after_opening_tag'           => true,
+            'no_useless_else'                       => true,
+            'no_useless_return'                     => true,
+            'ternary_to_null_coalescing'            => true,
+        ]
+    )
+    ->setFinder($finder);
